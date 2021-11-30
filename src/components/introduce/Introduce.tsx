5cd158958c0a78ba1b46d3d61b792e1c24ea0dd8
actbase <actbase@actbaseui-MacBookPro.local>
2021-11-30 23:00:20 +0900

    tag changed
    tag changed

--- src/components/introduce/Introduce.tsx
+++ src/components/introduce/Introduce.tsx
@@ -3,12 +3,12 @@ import styles from '../styles/introduce.module.css';
 
 const Introduce = () => {
   return (
-    <div className={styles.continer}>
+    <aside className={styles.continer}>
       <img className={styles.image} src="images/icon_light.jpeg" />
       <div className={styles.text_continer}>
         <div className={styles.content}>소처럼 열심히! 음머~</div>
       </div>
-    </div>
+    </aside>
   );
 };
 
